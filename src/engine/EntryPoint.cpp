19be07d526110c239d22c4720e7ed62da39178cf
Hein Dekkers <snuiterku@gmail.com>
2021-12-05 20:21:46 +0100

    Added File Logging

--- src/engine/EntryPoint.cpp
+++ src/engine/EntryPoint.cpp
@@ -1,6 +1,13 @@
 #include "log/Logger.h"
 
 int main(void) {
-	Phantom::Logger::SetPriority(Phantom::LogPriority::All);
-	Phantom::Logger::Trace("Hello World (%d, %s)", 5, "Hein Dekkers");
+	Phantom::Logger::EnableFileOutput();
+	Phantom::Logger::SetPriority(Phantom::All);
+	Phantom::Logger::Trace("This is a trace message");
+	Phantom::Logger::Info("This is an info message");
+	Phantom::Logger::Debug("This is a debug message");
+	Phantom::Logger::Warn("This is a warning message");
+	Phantom::Logger::Error("This is an error message");
+	Phantom::Logger::Fatal("This is a fatal message");
+	Phantom::Logger::DisableFileOutput();
 }
--- src/engine/log/Logger.h
+++ src/engine/log/Logger.h
@@ -14,68 +14,98 @@ namespace Phantom {
 	};
 
 	class Logger {
-	private:
-		static LogPriority m_Priority;
-
 	public:
 		static void SetPriority(LogPriority priority) {
 			m_Priority = priority;
 		}
 
+		static void EnableFileOutput() {
+			m_OutPath = "output.log";
+			enableFileOutput();
+		}
+
+		static void EnableFileOutput(const char* filepath) {
+			m_OutPath = filepath;
+			enableFileOutput();
+		}
+
+		static void DisableFileOutput() {
+			freeFile();
+		}
+
 		template<typename... Args>
 		static void Trace(const char* msg, Args... args) {
 			if (m_Priority < TracePriority) {
-				printf("[TRACE]\t");
-				printf(msg, args...);
-				printf("\n");
+				log("TRACE", FatalPriority, msg, args...);
 			}
 		}
 
 		template<typename... Args>
 		static void Debug(const char* msg, Args... args) {
 			if (m_Priority < DebugPriority) {
-				printf("[DEBUG]\t");
-				printf(msg, args...);
-				printf("\n");
+				log("DEBUG", FatalPriority, msg, args...);
 			}
 		}
 
 		template<typename... Args>
 		static void Info(const char* msg, Args... args) {
 			if (m_Priority < InfoPriority) {
-				printf("[INFO]\t");
-				printf(msg, args...);
-				printf("\n");
+				log("INFO", FatalPriority, msg, args...);
 			}
 		}
 
 		template<typename... Args>
 		static void Warn(const char* msg, Args... args) {
 			if (m_Priority < WarnPriority) {
-				printf("[WARNING]\t");
-				printf(msg, args...);
-				printf("\n");
+				log("WARN", FatalPriority, msg, args...);
 			}
 		}
 
 		template<typename... Args>
 		static void Error(const char* msg, Args... args) {
 			if (m_Priority < ErrorPriority) {
-				printf("[ERROR]\t");
-				printf(msg, args...);
-				printf("\n");
+				log("ERROR", FatalPriority, msg, args...);
 			}
 		}
 
 		template<typename... Args>
 		static void Fatal(const char* msg, Args... args) {
 			if (m_Priority < FatalPriority) {
-				printf("[FATAL]\t");
-				printf(msg, args...);
-				printf("\n");
+				log("FATAL", FatalPriority, msg, args...);
+			}
+		}
+	private:
+		static LogPriority m_Priority;
+		static const char* m_OutPath ;
+		static FILE* m_File;
+
+	private:
+		template<typename... Args>
+		static void log(const char* msgPriorStr, LogPriority p, const char* msg, Args... args) {
+			if (m_Priority < p) {
+				printf("[%s]\t%s\n", msgPriorStr, msg, args...);
+				if (m_File) {
+					fprintf(m_File, "[%s]\t%s\n", msgPriorStr, msg, args...);
+				}
 			}
 		}
 
+		static void enableFileOutput() {
+			if (m_File != 0) {
+				fclose(m_File);
+			}
+			m_File = fopen(m_OutPath, "a");
+			if (m_File == 0) {
+				printf("(Phantom::Logger) Failed to open file at: %s", m_OutPath);
+			}
+		}
+
+		static void freeFile() {
+			fclose(m_File);
+			m_File = 0;
+		}
 	};
-	LogPriority Logger::m_Priority = InfoPriority;
+	LogPriority Logger::m_Priority = All;
+	const char* Logger::m_OutPath = 0;
+	FILE* Logger::m_File;
 }
